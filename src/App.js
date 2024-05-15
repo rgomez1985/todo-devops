d6612e3281daf02d4312bad88a5d2765ae567d08
Ronal Gomez <ronalgomezmejia@hotmail.com>
2024-05-15 18:10:27 -0500

    se hacen pruebas TDD

--- src/App.js
+++ src/App.js
@@ -16,7 +16,11 @@ const initialToDoList = [
   { id: 3, status: false, title: "Mi Tercera Tarea" },
   { id: 4, status: true, title: "Tarea finalizada" },
   { id: 5, status: false, title: "Tarea por finalizar" },
+  { id: 6, status: false, title: "Comprar leche" },
+  { id: 7, status: false, title: "Leer un libro" },
 ];
+//initialToDoList.push({ id: 6, status: false, title: "Comprar leche" });
+
 // =============================================================================
 
 function App() {
@@ -87,7 +91,7 @@ function App() {
     <div className="container App">
       <br />
       <br />
-      <h2 data-testid="title">App for manage To Do list</h2>
+      <h2 data-testid="title">App for manage To Do list Ronal</h2>
       <br />
       <br />
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,29 +1,43 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App'; // Importa el componente que vas a probar
-import AddTaskForm from './components/AddTaskForm';
-import ToDo from './components/ToDo';
-import UpdateForm from './components/UpdateForm';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App"; // Importa el componente que vas a probar
+import AddTaskForm from "./components/AddTaskForm";
+import ToDo from "./components/ToDo";
+import UpdateForm from "./components/UpdateForm";
 
-describe('App', () => {
-  it('Renderiza los componentes sin error', () => {
+describe("App", () => {
+  it("Renderiza los componentes sin error", () => {
     render(<App />);
     render(<AddTaskForm />);
     render(<ToDo />);
     render(<UpdateForm />);
   });
 
-  it('Renderiza el título', () => {
+  it("Renderiza el título", () => {
     render(<App />);
 
-    const titleElement = screen.getByTestId('title');
-    expect(titleElement.textContent).toBe('App for manage To Do list');
+    const titleElement = screen.getByTestId("title");
+    expect(titleElement.textContent).toBe("App for manage To Do list Ronal");
   });
 
   it('Que contenga la tarea "Mi Primera Tarea"', () => {
     render(<App />);
 
-    const listItemElement = screen.getByText('Mi Primera Tarea');
+    const listItemElement = screen.getByText("Mi Primera Tarea");
+    expect(listItemElement).toBeDefined();
+  });
+
+  it('Debe contener la tarea "Comprar leche"', () => {
+    render(<App />);
+
+    const listItemElement = screen.getByText("Comprar leche");
+    expect(listItemElement).toBeDefined();
+  });
+
+  it('Debe contener la tarea "Leer un libro"', () => {
+    render(<App />);
+
+    const listItemElement = screen.getByText("Leer un libro");
     expect(listItemElement).toBeDefined();
   });
 });
